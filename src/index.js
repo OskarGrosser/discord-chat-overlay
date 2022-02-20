2255725c26ede483481158d243cbff69c1cad204
Oskar Grosser <oskargrossermail@yahoo.com>
2022-02-20 03:48:27 +0100

    Initial setup for Discord bot
    Remove middleware and Message API

--- src/index.js
+++ src/index.js
@@ -1,50 +1,26 @@
 import express from "express";
+import { Client, Intents } from "discord.js";
 import http from "http";
 import { Server } from "socket.io";
 import path from "path";
 import { fileURLToPath } from "url";
-import * as middleware from "./server/middleware.js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+const bot = new Client({
+  intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ]
+});
 
 app.use(express.static(path.join(__dirname, "server/static")));
-app.use(middleware.parseJson);
 
-app.use("/api/message", function(req, res) {
-  if (!req.body) {
-    return res.sendStatus(400);
-  }
-
-  switch (req.method) {
-  case "POST":
-    io.of("/message").emit("new", {
-      id: req.body.id,
-      author: req.body.author,
-      message: req.body.message
-    });
-    break;
-  case "PUT":
-    io.of("/message").emit("edit", {
-      id: req.body.id,
-      author: req.body.author,
-      message: req.body.message
-    });
-    break;
-  case "DELETE":
-    io.of("/message").emit("delete", { id: req.body.id });
-    break;
-  default:
-    res.setHeader("Allow", ["POST", "PUT", "DELETE"]);
-    return res.sendStatus(405);
-  }
-
-  res.sendStatus(202);
-});
+bot.on("messageCreate", message => {});
+bot.on("messageUpdate", (oldMessage, newMessage) => {});
+bot.on("messageDelete", message => {});
 
 server.listen(Number(process.env.PORT), () => {
   console.log("Listening on " + process.env.PORT);
 });
+bot.login(process.env.BOT_TOKEN);
